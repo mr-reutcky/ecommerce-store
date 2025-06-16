73a5605ae944d34976f3eadd0bc96db45bb281fa
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2025-06-16 10:05:32 -0500

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <></>
+    <>
+      <Header />
+      <main className="container">
+        <Routes>
+          <Route exct path='/' element={<Home />} />
+          <Route exact path='/product/:id' element={<Product />} />
+          <Route exact path='/cart' element={<Cart />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
