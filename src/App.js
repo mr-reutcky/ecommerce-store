e3c3abac7ae34c4f1c30ce2bc8dedfe4dc4cd5df
Samuel Reutcky <sreutcky@gmail.com>
2025-06-16 09:33:17 -0500

    basic Header done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Cart from './components/Cart';
+
+
+const navLinks = [
+  { name: 'Home', path: '/' },
+  { name: 'About', path: '/about' },
+  { name: 'Cart', path: '/cart' },
+];
 
 function App() {
   return (
-    <></>
+    <>
+      <Header links={navLinks} />
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} /> */}
+      </Routes></>
   );
 }
 
