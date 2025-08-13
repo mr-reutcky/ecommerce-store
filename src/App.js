7fe396a334dc0b74531450bec2c3f8add116225f
Samuel Reutcky <sreutcky@gmail.com>
2025-08-13 14:54:04 -0500

    Fixed errors from previos commits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,7 +8,6 @@ import { useState } from 'react';
 import Product from './pages/Product';
 import NotFoundPage from './pages/404'; 
 import CartPage from './pages/CartPage';
-// import Cart from './components/Cart';
 import CheckOut from "../src/pages/CheckOut";
 
 
@@ -22,19 +19,16 @@ const navLinks = [
 
 function App() {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const location = useLocation();
-
   return (
     <>
       <Header links={navLinks} onCartClick={() => { setDrawerOpen(true);}}/>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
-        <Route exact path="/productgit/:id" element={<Product />} />
+        <Route exact path="/product/:id" element={<Product />} />
         <Route exact path="/cart" element={<CartPage />} />
         <Route exact path="/CheckOut" element={<CheckOut/>} />
         <Route path="*" element={<NotFoundPage />} />
-        {/* <Route exact path="/cart" element={<Cart />} /> */}
         <Route exact path="/CheckOut" element={<CheckOut />} />
       </Routes>
       <CartDrawer open={drawerOpen} onClose={() => setDrawerOpen(false)} />
--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import '../css/CheckOut.css';
-import CountrySelector from "../components/CountrySelector";
+import '../css/Checkout.css';
 import { useForm } from "react-hook-form";
 import { useCart } from '../components/CartContext';
 
 function Checkout() {
-     const { state: { items } } = useCart();
-
- 
+  const { state: { items } } = useCart();
   const totalItems = items.reduce((acc, item) => acc + item.qty, 0);
 
-  
   const totalPrice = items.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2);
   const {
     register,
@@ -21,43 +17,30 @@ function Checkout() {
   const onSubmit = (data) => {
     console.log("Form Data:", data);
   };
-
   return (
     <div className="checkout-page grid container">
-   
       <div className="checkout-form left">
         <h2>Checkout</h2>
         <h3>Choose a payment Method</h3>
-
         <div className="payment-options">
           <div className="payment-card">Visa **** 1234</div>
           <div className="payment-card">MasterCard **** 5678</div>
           <div className="payment-card">PayPal Account</div>
             <div className="payment-card">Apple pay</div>
         </div>
-
-        {/* FORM */}
         <form onSubmit={handleSubmit(onSubmit)} className="checkout-form-section">
-         
-
           <div className="form-group">
             <label>Full Name</label>
             <input type="text" {...register("fullName", { required: true })} />
-           
           </div>
- <div className="form-group">
+          <div className="form-group">
             <label>Full Address</label>
             <input type="text" {...register("Address", { required: true })} />
-          
           </div>
-        
-
           <div className="form-group">
             <label>Phone Number</label>
             <input type="tel" {...register("phone", { required: true })} />
-            
           </div>
-
           <div className="form-group">
             <label>Email</label>
             <input
@@ -69,16 +52,11 @@ function Checkout() {
             />
             {errors.email && <p className="error">Valid email is required</p>}
           </div>
-
-
         </form>
       </div>
-
-      
       <div className="checkout-summary right">
         <h3 className="summary">Order Summary</h3>
       <h4 className = "items">Total Items: {totalItems}</h4>
-
       <h4>Total Price: ${totalPrice}</h4>
       <br></br>
       <div className='form-group'>
